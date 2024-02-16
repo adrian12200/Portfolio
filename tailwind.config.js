2de3cd2a1a896399d2e05a92e1751bfdd02730ee
adrian12200 <bontigaoadrian@gmail.com>
2024-02-16 19:22:56 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        neutral: '#0a0a0a',
+        gray: '#52525b',
+        white: '#fafafa',
+        primary: {
+          100: '#00abcf',
+          200: '#DCF2F1',
+          300: '#7FC7D9',
+          400: '#365486',
+          500: '#0F1035',
+          600: '#38576c',
+        }
+      }
+    }
+  }
+};
